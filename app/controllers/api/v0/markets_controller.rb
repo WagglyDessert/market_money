8355f90bd17ff90248636e5c34a026c4f389c49e
Nathan Trautenberg <Natester123@mac.com>
2023-12-05 14:59:30 -0700

    updated tests for new serializers and error messages. created error_message poros.

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,14 +1,18 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
-    render json: Market.all
+    render json: MarketSerializer.new(Market.all)
   end
 
   def show
-    begin
-      market = Market.find(params[:id])
-      render json: market
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }, status: :not_found
-    end
+    render json: MarketSerializer.new(Market.find(params[:id]))
+  end
+
+  private
+ 
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+      .serialize_json, status: :not_found
   end
 end
---
+++ app/serializers/market_serializer.rb
@@ -0,0 +1,14 @@
+class MarketSerializer
+  include JSONAPI::Serializer
+  attributes :name, :street, :city, :county, :state, :zip, :lat, :lon
+
+  has_many :vendors
+
+  attribute :vendor_count do |object|
+    object.vendors.count
+  end
+
+  attribute :active do
+    true
+  end
+end
