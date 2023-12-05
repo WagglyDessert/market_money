e62824222f3a0a0032670a42dd560db261e499e4
Nathan Trautenberg <Natester123@mac.com>
2023-12-05 14:10:19 -0700

    made factories for vendors and market_vendors.

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -4,17 +4,6 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   def show
-    # @market = MarketFacade.new(params[:market_id])
-
-     # markets = Faraday.new(url: "http://localhost:3000")
-    # response = markets.get("/api/v0/markets.json")
-    # data = JSON.parse(response.body, symbolize_names: true)
-
-    # members = data[:results][0][:members]
-
-    # found_members = members.find_all {|m| m[:last_name] == params[:search]}
-    # @member = found_members.first
-    # render "welcome/index"
     begin
       market = Market.find(params[:id])
       render json: market
